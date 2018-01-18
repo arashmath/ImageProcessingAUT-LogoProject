fb9d1bd07ab074dbcd3885a58ef4692cce86b639
unknown <arashmath@aut.ac.ir>
2018-01-18 22:07:53 +0330

    Corrections applied

--- LogoOnImage.m
+++ LogoOnImage.m
@@ -6,10 +6,10 @@ GrayscaleImage = rgb2gray(RGBimage);
 
 RGBlogo = imread('logo.png');
 GrayscaleLogo = rgb2gray(RGBlogo);
-GrayscaleLogo = imresize(GrayscaleLogo, 0.2, 'bilinear');
+GrayscaleLogo = imresize(GrayscaleLogo, 0.3, 'bilinear');
 
 [m,n] = size(GrayscaleLogo);
-alpha = 0.8;
+alpha = im2double(GrayscaleLogo)<0.5;
 
 ResultImage = GrayscaleImage;
 ResultImage(1:m,1:n) = uint8(alpha .* double(GrayscaleLogo) + (1-alpha) .* double(GrayscaleImage(1:m,1:n)));
